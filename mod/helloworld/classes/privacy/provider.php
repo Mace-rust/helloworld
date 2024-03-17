e8ef7b28b99f1a8ce9a9a51d52de048c37968284
Victor <golovenko.vitya.05@mail.ru>
2024-03-18 05:31:33 +0700

    тоже файлы приветствия

---
+++ mod/helloworld/classes/privacy/provider.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace mod_helloworld\privacy;
+
+defined('MOODLE_INTERNAL') || die();
+
+class provider implements \core_privacy\local\request\provider
+{
+
+    public static function get_reason(): string
+    {
+        return 'Plugin requires some user data to display greeting message.';
+    }
+
+    public static function get_contexts(): array
+    {
+        return [\context_system::instance()];
+    }
+
+    public static function export_user_data(\core_privacy\local\request\writer $writer, \core_privacy\local\request\user_preference $preference): void
+    {
+        // No user data to export
+    }
+
+    public static function delete_data_for_user(\core_privacy\local\request\user_preference $preference): bool
+    {
+        // No user data to delete
+        return true;
+    }
+}
