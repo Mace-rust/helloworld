057e549d59909b0814e20478d26e3a9e003b8cf1
Виктор А. Головенко <vga@2i.tusur.local>
2024-04-04 19:24:48 +0700

    Описание плагина

--- lang/ru/helloworld.php
+++ lang/ru/helloworld.php
@@ -3,4 +3,4 @@ declare(strict_types=1);
 
 $string['modulename'] = 'Привет Мир!';
 $string['pluginname'] = 'Привет Мир!';
-$string['helloworld'] = 'Hello World!';
+$string['modulename_help'] = 'Плагин "' . $string['pluginname'] . '" - это простой и легкой ресурс, который позволяет выразить приветствие вашим пользователям =).';
--- version.php
+++ version.php
@@ -1,9 +1,11 @@
 <?php
 declare(strict_types=1);
 
+defined('MOODLE_INTERNAL') || die();
+
 /** @var stdClass $plugin */
 $plugin->component = 'mod_helloworld';
-$plugin->version = 2024040301;
+$plugin->version = 2024040305;
 $plugin->requires = 2021051700; // Moodle 3.11
 $plugin->type = 'resource';
 $plugin->icon = 'pix/icon.png'; // Путь к изображению иконки вашего плагина
