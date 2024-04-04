8eb5ead360ab71497436211dd07b74d355247085
Виктор А. Головенко <vga@2i.tusur.local>
2024-04-04 18:44:36 +0700

    рабочая иконка + удалён мусор

--- version.php
+++ version.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_helloworld';
-$plugin->version = 2024040300;
+$plugin->version = 2024040301;
 $plugin->requires = 2021051700; // Moodle 3.11
 $plugin->type = 'resource';
+$plugin->icon = 'pix/icon.png'; // Путь к изображению иконки вашего плагина
