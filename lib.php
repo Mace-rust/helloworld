96ae30f9755186325b55efcc8f659109c38717d4
Виктор А. Головенко <vga@2i.tusur.local>
2024-04-04 10:15:34 +0700

    resource working

--- lib.php
+++ lib.php
@@ -8,22 +8,22 @@ defined('MOODLE_INTERNAL') || die();
  * @param string $feature FEATURE_xx constant for requested feature
  * @return mixed True if module supports feature, false if not, null if doesn't know or string for the module purpose.
  */
-//function helloworld_supports($feature) {
-//    switch($feature) {
-//        case FEATURE_MOD_ARCHETYPE:           return MOD_ARCHETYPE_RESOURCE;
-//        case FEATURE_GROUPS:                  return false;
-//        case FEATURE_GROUPINGS:               return false;
-//        case FEATURE_MOD_INTRO:               return false; //ввод инфы не поддерживаем
-//        case FEATURE_COMPLETION_TRACKS_VIEWS: return true; //отслеживание просмотров
-//        case FEATURE_GRADE_HAS_GRADE:         return false;
-//        case FEATURE_GRADE_OUTCOMES:          return false;
-//        case FEATURE_BACKUP_MOODLE2:          return true; // резервное копирование
-//        case FEATURE_SHOW_DESCRIPTION:        return true; // описание плагина
-//        case FEATURE_MOD_PURPOSE:             return MOD_PURPOSE_CONTENT;
-//
-//        default: return null;
-//    }
-//}
+function helloworld_supports($feature) {
+    switch($feature) {
+        case FEATURE_MOD_ARCHETYPE:           return MOD_ARCHETYPE_RESOURCE;
+        case FEATURE_GROUPS:                  return false;
+        case FEATURE_GROUPINGS:               return false;
+        case FEATURE_MOD_INTRO:               return false; //ввод инфы не поддерживаем
+        case FEATURE_COMPLETION_TRACKS_VIEWS: return true; //отслеживание просмотров
+        case FEATURE_GRADE_HAS_GRADE:         return false;
+        case FEATURE_GRADE_OUTCOMES:          return false;
+        case FEATURE_BACKUP_MOODLE2:          return true; // резервное копирование
+        case FEATURE_SHOW_DESCRIPTION:        return true; // описание плагина
+        case FEATURE_MOD_PURPOSE:             return MOD_PURPOSE_CONTENT;
+
+        default: return null;
+    }
+}
 
 function helloworld_add_instance($helloworld) {
     $helloworld->id = insert_record('helloworld', $helloworld);
