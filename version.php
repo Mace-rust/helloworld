5df8ce89383eaaaacf291c97ec7577d26024c9d2
Виктор А. Головенко <vga@2i.tusur.local>
2024-04-09 20:07:48 +0700

    + capabilities add:instance DB

--- version.php
+++ version.php
@@ -5,5 +5,5 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_helloworld';
-$plugin->version = 2024040904;
+$plugin->version = 2024040906;
 $plugin->requires = 2021051700; // Moodle 3.11
