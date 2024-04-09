ac4f1e8569fc6f943394ab120f67880c58c29241
Виктор А. Головенко <vga@2i.tusur.local>
2024-04-09 17:47:45 +0700

    убран локал либ + фикшен версион + либ

--- lib.php
+++ lib.php
@@ -8,20 +8,30 @@ defined('MOODLE_INTERNAL') || die();
  * @param string $feature FEATURE_xx constant for requested feature
  * @return mixed True if module supports feature, false if not, null if doesn't know or string for the module purpose.
  */
-function helloworld_supports($feature) {
-    switch($feature) {
-        case FEATURE_MOD_ARCHETYPE:           return MOD_ARCHETYPE_RESOURCE;
-        case FEATURE_GROUPS:                  return false;
-        case FEATURE_GROUPINGS:               return false;
-        case FEATURE_MOD_INTRO:               return false; //ввод инфы не поддерживаем
-        case FEATURE_COMPLETION_TRACKS_VIEWS: return false; //отслеживание просмотров
-        case FEATURE_GRADE_HAS_GRADE:         return false;
-        case FEATURE_GRADE_OUTCOMES:          return false;
-        case FEATURE_BACKUP_MOODLE2:          return false; // резервное копирование
-        case FEATURE_SHOW_DESCRIPTION:        return true; // описание плагина
-        //case FEATURE_MOD_PURPOSE:             return MOD_PURPOSE_CONTENT;
+function helloworld_supports($feature)
+{
+    switch ($feature) {
+        case FEATURE_MOD_ARCHETYPE:
+            return MOD_ARCHETYPE_RESOURCE;
+        case FEATURE_GROUPS:
+            return false;
+        case FEATURE_GROUPINGS:
+            return false;
+        case FEATURE_MOD_INTRO:
+            return false; //ввод инфы не поддерживаем
+        case FEATURE_COMPLETION_TRACKS_VIEWS:
+            return false; //отслеживание просмотров
+        case FEATURE_GRADE_HAS_GRADE:
+            return false;
+        case FEATURE_GRADE_OUTCOMES:
+            return false;
+        case FEATURE_BACKUP_MOODLE2:
+            return false; // резервное копирование
+        case FEATURE_SHOW_DESCRIPTION:
+            return true; // описание плагина
 
-        default: return null;
+        default:
+            return null;
     }
 }
 
--- version.php
+++ version.php
@@ -5,7 +5,5 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'mod_helloworld';
-$plugin->version = 2024040801;
+$plugin->version = 2024040900;
 $plugin->requires = 2021051700; // Moodle 3.11
-$plugin->type = 'resource';
-$plugin->icon = 'pix/icon.png'; // Путь к изображению иконки вашего плагина
