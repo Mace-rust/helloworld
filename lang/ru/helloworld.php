47a4c84335c5da252068c13f2d73b48c89803bcc
Виктор А. Головенко <vga@2i.tusur.local>
2024-04-09 19:47:35 +0700

    +modulenameplural

--- lang/ru/helloworld.php
+++ lang/ru/helloworld.php
@@ -5,4 +5,5 @@ defined('MOODLE_INTERNAL') || die();
 
 $string['modulename'] = 'Привет Мир!';
 $string['pluginname'] = 'Привет Мир!';
+$string['modulenameplural'] = 'Привет Мир!';
 $string['modulename_help'] = 'Плагин "' . $string['pluginname'] . '" - это простой и легкой ресурс, который позволяет выразить приветствие вашим пользователям =).';
