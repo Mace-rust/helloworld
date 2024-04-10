ec52be34f55a9e2a066dbe462d2f3565cfd77993
Виктор А. Головенко <vga@2i.tusur.local>
2024-04-10 10:14:33 +0700

    + доступ студентам для изменения БД

--- db/access.php
+++ db/access.php
@@ -5,21 +5,24 @@ defined('MOODLE_INTERNAL') || die;
 $capabilities = array(
     'mod/helloworld:view' => array(
         'captype' => 'read',
-        'contextlevel' => CONTEXT_MODULE,
+        'contextlevel' => CONTEXT_MODULE, // разрешение применяется на уровне модуля курса
         'archetypes' => array(
+            // гость + пользователь = разрешение на просмотр модуля
             'guest' => CAP_ALLOW,
             'user' => CAP_ALLOW,
         )
     ),
 
     'mod/helloworld:addinstance' => array(
-        'riskbitmask' => RISK_XSS,
+        'riskbitmask' => RISK_XSS, // уязвимость к XSS атакам
 
         'captype' => 'write',
-        'contextlevel' => CONTEXT_COURSE,
+        'contextlevel' => CONTEXT_COURSE,  // разрешение применяется на уровне курса
         'archetypes' => array(
+            //только преподаватели + администраторы = разрешение на добавление новых экземпляров модуля
             'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
+            'manager' => CAP_ALLOW,
+            'student' => CAP_ALLOW, // добавил потому что может не было прав чтобы нажать кнопку о выполнении курса у студента
         ),
         'clonepermissionsfrom' => 'moodle/course:manageactivities'
     ),
